8fc57a137a4037d5268fad60d6122d39e4aa3cc8
JohnAndrewFonseca <johnandrewfonseca@gmail.com>
2026-04-24 23:44:38 +0800

    Change description in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LH-Connect",
-  description: "A beautiful login page built with Next.js",
+  description: "A Unified Management and Information System for Automating Monthly Dues and Resident Financial Analytics",
 };
 
 export default function RootLayout({
